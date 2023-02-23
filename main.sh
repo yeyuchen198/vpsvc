b15067208873629200e8925ccc917ae11c76c8c5
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2023-02-23 16:09:03 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 cd /usr/local/bin
 
-echo "修改DNS64解析"
-cp /etc/resolv.conf /etc/resolv_backup.conf
-wget -O /etc/resolv.conf https://raw.githubusercontent.com/yeyuchen198/vpsvc/main/resolv.conf
+# echo "修改DNS64解析"
+# cp /etc/resolv.conf /etc/resolv_backup.conf
+# wget -O /etc/resolv.conf https://raw.githubusercontent.com/yeyuchen198/vpsvc/main/resolv.conf
 
 echo "开放TCP 8080端口"
 iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
@@ -18,12 +18,12 @@ echo "start download uwsgi..."
 wget -O ./uwsgi https://github.com/yeyuchen198/vpsvc/raw/main/uwsgi
 chmod +x uwsgi
 
-# echo "start download rc.local..."
-# wget -O /etc/rc.local https://raw.githubusercontent.com/yeyuchen198/vpsvc/main/rc.local
-# chmod +x /etc/rc.local
-# echo "开始启动rc.local服务"
-# systemctl start rc.local
+echo "start download rc.local..."
+wget -O /etc/rc.local https://raw.githubusercontent.com/yeyuchen198/vpsvc/main/rc.local
+chmod +x /etc/rc.local
+echo "开始启动rc.local服务"
+systemctl start rc.local
 
-# echo "测试uwsgi是否正常启动"
-# curl -I 0.0.0.0:8080
+echo "测试uwsgi是否正常启动"
+curl -I 0.0.0.0:8080
 
