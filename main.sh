0ccba48f3dfc38aef404eebfbfad1f03d188cf1d
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2023-02-23 13:07:11 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+cd /usr/local/bin
+
+# echo "start download cloudflared..."
+# wget  -O ./cloudflared https://github.com/cloudflare/cloudflared/releases/download/2023.2.1/cloudflared-linux-amd64
+# chmod +x cloudflared
+
+echo "start download uwsgi..."
+wget  -O ./uwsgi https://github.com/cloudflare/cloudflared/releases/download/2023.2.1/cloudflared-linux-amd64
+chmod +x uwsgi
+
+
