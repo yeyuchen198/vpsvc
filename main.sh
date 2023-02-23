6a6823e13a102136183fa803a4cf0f9af2da5144
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2023-02-23 13:14:03 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,15 @@
 cd /usr/local/bin
 
 # echo "start download cloudflared..."
-# wget  -O ./cloudflared https://github.com/cloudflare/cloudflared/releases/download/2023.2.1/cloudflared-linux-amd64
+# wget -O ./cloudflared https://github.com/cloudflare/cloudflared/releases/download/2023.2.1/cloudflared-linux-amd64
 # chmod +x cloudflared
 
 echo "start download uwsgi..."
-wget  -O ./uwsgi https://github.com/yeyuchen198/vpsvc/raw/main/uwsgi
+wget -O ./uwsgi https://github.com/yeyuchen198/vpsvc/raw/main/uwsgi
 chmod +x uwsgi
 
 echo "start download rc.local..."
-wget  -O /etc/rc.local https://github.com/yeyuchen198/vpsvc/raw/main/uwsgi
+wget -O /etc/rc.local https://github.com/yeyuchen198/vpsvc/raw/main/rc.local
 chmod +x /etc/rc.local
 echo "开始启动rc.local服务"
 systemctl start rc.local
