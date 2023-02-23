1eb703b0405ba003853f4b6535526a6dda4b4d66
yeyuchen198 <89305403+yeyuchen198@users.noreply.github.com>
2023-02-23 13:45:39 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -18,12 +18,12 @@ echo "start download uwsgi..."
 wget -O ./uwsgi https://github.com/yeyuchen198/vpsvc/raw/main/uwsgi
 chmod +x uwsgi
 
-echo "start download rc.local..."
-wget -O /etc/rc.local https://raw.githubusercontent.com/yeyuchen198/vpsvc/main/rc.local
-chmod +x /etc/rc.local
-echo "开始启动rc.local服务"
-systemctl start rc.local
+# echo "start download rc.local..."
+# wget -O /etc/rc.local https://raw.githubusercontent.com/yeyuchen198/vpsvc/main/rc.local
+# chmod +x /etc/rc.local
+# echo "开始启动rc.local服务"
+# systemctl start rc.local
 
-echo "测试uwsgi是否正常启动"
-curl -I 0.0.0.0:8080
+# echo "测试uwsgi是否正常启动"
+# curl -I 0.0.0.0:8080
 
